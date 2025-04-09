15e9ac6ac3c4e3f3bd56e601068794aeb59df45a
Katherina <misskatherina.m@gmail.com>
2025-04-09 01:33:55 -0400

    Enemy stay in the camera range

--- Assets/Scripts/EnemySystem.cs
+++ Assets/Scripts/EnemySystem.cs
@@ -7,18 +7,41 @@ public class EnemySystem : MonoBehaviour
 {
     public float moveSpeed = 2f;
     public float changeDirectionTime = 2f;
+    public Camera mainCam;
 
     private Vector2 moveDirection;
     private float timer = 0f;
-   
+    
+    private Vector2 minBound;
+    private Vector2 maxBound;
+
     void Start()
     {
         PickNewDirection();
+        mainCam = Camera.main;
+
+        //Bounds on the screen
+        Vector3 bottomLeft = mainCam.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 topRight = mainCam.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        minBound = new Vector2(bottomLeft.x, bottomLeft.y);
+        maxBound = new Vector2(topRight.x, topRight.y);
     }
 
     
     void Update()
     {
+        Vector3 nextPos = transform.position + (Vector3)(moveDirection * moveSpeed * Time.deltaTime);
+
+        //Check if next position is outside of the camera
+        if (nextPos.x < minBound.x || nextPos.x > maxBound.x)
+        {
+            moveDirection.x *= -1; //Flip x direction
+        }
+        if (nextPos.y < minBound.y || nextPos.y > maxBound.y)
+        {
+            moveDirection.y *= -1; //Flip y direction
+        }
+
         //Move enemy in the chosen direction
         transform.Translate (moveDirection * moveSpeed * Time.deltaTime);
 
