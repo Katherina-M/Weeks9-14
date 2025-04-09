5e2c04830ddb7ce20e7996d232fa2cee44a73147
Katherina <misskatherina.m@gmail.com>
2025-04-09 04:59:39 -0400

    Check player distance and orb distance, make orb disappear!

--- Assets/Scripts/InvisibilityItem.cs
+++ Assets/Scripts/InvisibilityItem.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 
 public class InvisibilityItem : MonoBehaviour
 {
+    public Transform player;
+    public float pickupRange = 1f;
+
     private bool hasSetPosition = false;
+    private Vector2 minBound, maxBound;
+    private Camera mainCam;
 
     void Start()
     {
+        mainCam = Camera.main;
+
+        // Get the screen bounds in world space
+        Vector3 bottomLeft = mainCam.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 topRight = mainCam.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        minBound = new Vector2(bottomLeft.x, bottomLeft.y);
+        maxBound = new Vector2(topRight.x, topRight.y);
+
+        //Place the orb randomly
         if (!hasSetPosition)
         {
-            float x = Mathf.Round(Random.Range(-8f, 8f));
-            float y = Mathf.Round(Random.Range(-4f, 4f));
+            float x = Random.Range(minBound.x + 0.5f, maxBound.x - 0.5f);
+            float y = Random.Range(minBound.y + 0.5f, maxBound.y - 0.5f);
             transform.position = new Vector2(x, y);
             hasSetPosition = true;
+        }
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            float distance = Vector2.Distance(transform.position, player.position);
+            if (distance <= pickupRange)
+            {
+                Destroy(gameObject); // Bye bye, orb!
+            }
         }
     }
 }
