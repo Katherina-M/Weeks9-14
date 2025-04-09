371cb94eb57818d189c279dc2ba7353e79fa340d
Katherina <misskatherina.m@gmail.com>
2025-04-09 04:51:32 -0400

    The orb shows in different location when the game start

--- Assets/Scripts/InvisibilityItem.cs
+++ Assets/Scripts/InvisibilityItem.cs
@@ -5,5 +5,16 @@ using UnityEngine;
 
 public class InvisibilityItem : MonoBehaviour
 {
- 
+    private bool hasSetPosition = false;
+
+    void Start()
+    {
+        if (!hasSetPosition)
+        {
+            float x = Mathf.Round(Random.Range(-8f, 8f));
+            float y = Mathf.Round(Random.Range(-4f, 4f));
+            transform.position = new Vector2(x, y);
+            hasSetPosition = true;
+        }
+    }
 }
