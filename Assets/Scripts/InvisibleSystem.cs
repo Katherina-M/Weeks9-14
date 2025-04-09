84eae74f423d4023f9c2d8b016206d852f8937de
Katherina <misskatherina.m@gmail.com>
2025-04-09 00:59:08 -0400

    Player Movement Set

---
+++ Assets/Scripts/InvisibleSystem.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class InvisibleSystem : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        Vector2 direction = Vector2.zero;
+
+        //Movement Keys
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            direction += Vector2.up;
+            Debug.Log("W key held");
+        }
+        
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector2.right;
+        }
+
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+}
